593e3d84893550fdbea91cb2357304e03e7a6e9f
Denis Safronov <safron_dz@mail.ru>
2020-11-01 14:38:41 +0300

    Add controller "projects" and views pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,35 @@
 class ProjectsController < ApplicationController
 	def index
-		render json: {message: "ok"}
+		# render json: {message: "ok"}
+		@tasks = Task.all
 	end
 
 	def new
+		@task = Task.new
+	end
 
+	def show
+		@task = Task.find(params[:id])
+	end
+
+	def create
+		@task = Task.new(parameters)
+		if true
+		@task.save
+		redirect_to 'index'
+	end
+	end
+
+	def edit
+		@task = Task.find(params[:id])
+	end
+
+	def update
+		@task = Task.find(params[:id])
+	end
+	private
+
+	def parameters
+		params.require(:task).permit(:title)
 	end
 end
